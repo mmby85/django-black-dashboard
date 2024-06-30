72f66debc8999fa1155d034598222e507a2490c9
Ahmedbencheikhbrahim1 <ahmed.bencheikhbrahim@esprit.tn>
2024-06-30 12:01:14 +0100

    adding functions for visualisation

--- admin_black/KPIs.py
+++ admin_black/KPIs.py
@@ -85,7 +85,8 @@ def ventes_par_client(invoices_df):
 # CA par produit : 
 def top_products_sold(product_details_df):
     product_sales = product_details_df.groupby('product_name').agg(total_quantity=('quantity', 'sum'))
-    top_selling_products = product_sales.sort_values(by='total_quantity', ascending=False).head(10)
+    top_selling_products = product_sales.sort_values(by='total_quantity', ascending=False)
+    top_selling_products =top_selling_products
     return top_selling_products
 
 # revenu par produit
@@ -110,9 +111,89 @@ def sales_per_employee(invoices_df):
 #------------------------------------------------------------------------------
 #-------------------KPIs Page FACTURES-----------------------------------------
 
-
+# nombre de factures par mois , annee , semestre :
 def nombre_factures(group_by):
     freq_map = {"month": "M", "quarter": "Q", "year": "A"}
     grouped_df = invoices_df.groupby(pd.Grouper(key="date", freq=freq_map[group_by])).size().reset_index(name="number_of_invoices")
     return grouped_df
+
+# CA par client :
+def ventes_par_clients(invoices_df):
+  employee_sales = invoices_df.groupby('partner_name').agg(total_revenue=('amount_total', 'sum')).reset_index()
+  employee_sales = employee_sales.sort_values(by='total_revenue', ascending=False).set_index('partner_name')
+  return employee_sales
+
+# client sales :
+def client_sales_details(client_name, start_date=None, end_date=None, period="month"):
+
+    if period not in ["month", "quarter", "year"]:
+        raise ValueError("Invalid period. Please choose from 'month', 'quarter', or 'year'.")
+
+    filtered_df = invoices_df.loc[invoices_df["partner_name"] == client_name]
+    if start_date is not None:
+        filtered_df = filtered_df.loc[filtered_df["date"] >= start_date]
+    if end_date is not None:
+        filtered_df = filtered_df.loc[filtered_df["date"] <= end_date]
+
+    freq_map = {"month": "M", "quarter": "Q", "year": "A"}
+    grouped_df = filtered_df.groupby(pd.Grouper(key="date", freq=freq_map[period])).agg({"amount_total": "sum"})
+    grouped_df.columns = ["sales"]
+    grouped_df = grouped_df.reset_index()
+    return grouped_df
+# retation rate for periode 
+def calculate_client_retention_rate(invoices_df, period):
+
+    # Ensure the 'date' column is in datetime format
+    invoices_df['date'] = pd.to_datetime(invoices_df['date'])
+
+    # Extract the period from the date column
+    if period == 'year':
+        invoices_df['period'] = invoices_df['date'].dt.year
+    elif period == 'quarter':
+        invoices_df['period'] = invoices_df['date'].dt.to_period('Q')
+    elif period == 'month':
+        invoices_df['period'] = invoices_df['date'].dt.to_period('M')
+    else:
+        raise ValueError("Invalid period. Choose from 'year', 'quarter', or 'month'.")
+    period_partner_counts = invoices_df.groupby('period')['partner_id'].nunique()
+    repeat_clients = invoices_df.groupby('period')['partner_id'].apply(lambda x: x.duplicated().sum())
+    retention_rate = (repeat_clients / period_partner_counts).mean() * 100
+    return retention_rate
+
+
+# retention rate over time:
+def retention_rate_over_time(invoices_df, period):
+    invoices_df['date'] = pd.to_datetime(invoices_df['date'])
+
+    # Extract the period from the date column
+    if period == 'year':
+        invoices_df['period'] = invoices_df['date'].dt.year
+        all_periods = pd.Series(range(invoices_df['period'].min(), invoices_df['period'].max() + 1)) # Use 'period' here
+
+    elif period == 'quarter':
+        invoices_df['period'] = invoices_df['date'].dt.to_period('Q')
+        all_periods = pd.period_range(invoices_df['period'].min(), invoices_df['period'].max(), freq='Q')
+
+    elif period == 'month':
+        invoices_df['period'] = invoices_df['date'].dt.to_period('M')
+        all_periods = pd.period_range(invoices_df['period'].min(), invoices_df['period'].max(), freq='M') 
+
+    else:
+        raise ValueError("Invalid period. Choose from 'year', 'quarter', or 'month' ")
+
+    
+    # Group by period and partner_id, and count the number of unique partner_ids
+    period_partner_counts = invoices_df.groupby('period')['partner_id'].nunique()
+
+    # Calculate the number of repeat clients in each period
+    repeat_clients = invoices_df.groupby('period')['partner_id'].apply(lambda x: x.duplicated().sum())
+
+    # Calculate the Client Retention Rate for each period
+    retention_rate_series = (repeat_clients / period_partner_counts) * 100
+
+    # Fill missing periods with 0 retention rate
+    retention_rate_series = retention_rate_series.reindex(all_periods, fill_value=0)
+
+    return retention_rate_series
+
 
--- admin_black/data_processing.py
+++ admin_black/data_processing.py
@@ -35,8 +35,30 @@ cancelled_sale_orders = len(cancelled_sale_orders_ids)
 sale_orders_with_invoice = len(invoiced_sale_orders_ids)
 sale_orders_to_invoiced = len(to_invoice_sale_orders_ids)
 
-employee_sales1 = ventes_par_client(invoices_df)
 
 facture_par_mois = nombre_factures("month")
 facture_par_semestre =nombre_factures("quarter")
 facture_par_annee = nombre_factures("year")
+CA_client = ventes_par_clients(invoices_df)
+client_name = 'Client X' # must be givin as an input 
+client_sales = client_sales_details(client_name, start_date=None, end_date=None, period="month")
+# nombre de transaction par clients :
+nb_fac_par_client = invoices_df.groupby('partner_name').size()
+#Top 10 Clients générant le moins revenue :
+partner_sales = ventes_par_clients(invoices_df)
+top_revenue_par_client= partner_sales.sort_values(by="total_revenue", ascending=False).head(10)
+#Top 10 Clients générant le plus revenue :
+moins_revenue_par_client = partner_sales.sort_values(by="total_revenue" , ascending=True).head(10)
+# retention rate detailed : 
+retention_mois = retention_rate_over_time(invoices_df, period='month')
+retention_semstre = retention_rate_over_time(invoices_df, period='quarter')
+retention_annee = retention_rate_over_time(invoices_df, period='year')
+
+rt_mois = calculate_client_retention_rate(invoices_df, period='month')
+rt_semestre = calculate_client_retention_rate(invoices_df, period ='quarter')
+rt_annee = calculate_client_retention_rate(invoices_df, period ='year')
+
+
+#-------------------KPIs Page PRODUCTS (formations ) -----------------------------------------
+CA_par_produit1 = top_products_sold(product_details_df)
+revenu_produit1 = Revenu_par_produit(product_details_df)
